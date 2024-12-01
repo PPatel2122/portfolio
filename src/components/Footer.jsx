4c67a028ade97ad10757e370dabfeca5808b48f3
PPatel2122 <pp467354@gmail.com>
2024-12-01 19:07:43 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return(
         <div class="text-center p-6 bg-black footer">
         <span class="text-white">&copy; 2024 Copyright - </span>
-        <a class="link-underline text-white font-bold" href="">Pawan Patel</a>
+        <a class="link-underline text-white font-bold" href="https://www.instagram.com/p__patel_27/">Pawan Patel</a>
       </div>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
       <div>
-        <h1 className="text-5xl font-signature ml-2"><a className="link-underline link-underline-black" href="" target="_blank" rel="noreferrer">PAWAN</a></h1>
+        <h1 className="text-5xl font-signature ml-2"><a className="link-underline link-underline-black" href="https://www.instagram.com/p__patel_27/" target="_blank" rel="noreferrer">PAWAN</a></h1>
       </div>
 
       <ul className="hidden md:flex">
